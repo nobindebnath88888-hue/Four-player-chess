6bff8fdd1fc6ff333873175f4cbd3ac73dc945f5
nobindebnath88888-hue <nobindebnath88888@gmail.com>
2026-03-05 16:46:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,7 @@
     measurementId: "G-S1C4GPJFXT"
   };
 
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
